d79a292207b2779d8b6b1c194c4dedf292e59368
bleachagent18T <pj7699774@gmail.com>
2025-07-19 11:25:36 +0530

    Added my love for anime

--- main.c
+++ main.c
@@ -2,5 +2,8 @@
 
 int main() {
     printf("Hello, World! \n");
+    printf("anime is the greatest thing ever\n");
+    printf("I love anime\n");
+    printf("I love anime so much\n");   
     return 0;
 }
